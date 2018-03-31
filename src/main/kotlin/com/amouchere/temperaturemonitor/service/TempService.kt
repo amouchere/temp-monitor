b5af2411633fef5ab139ec75a093a790b3fc242c
a.mouchere <a.mouchere@gmail.com>
2018-03-31 22:06:22 +0200

    set history to 30

--- src/main/kotlin/com/amouchere/temperaturemonitor/service/TempService.kt
+++ src/main/kotlin/com/amouchere/temperaturemonitor/service/TempService.kt
@@ -1,17 +1,21 @@
 package com.amouchere.temperaturemonitor.service
 
 import com.amouchere.temperaturemonitor.domain.TempData
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TempService {
+
+    @Value("\${maxHistory}")
+    private var maxHistory: Int? = null
 
     private val list = mutableListOf<TempData>()
 
     fun getTemp() = list
 
     fun addTemp(d: TempData): List<TempData> {
-        if (list.size > 10) list.removeAt(0)
+        if (list.size > this!!.maxHistory!!) list.removeAt(0)
         list.add(d)
         return list
     }
